c8f8526b1744745990e6549eb451470033ebdaac
Brett Haymaker <brett.haymaker@gmail.com>
2019-09-03 19:23:47 -0500

    lesson-10-programmatic-thinking

---
+++ 10-programmatic-thinking/curt/index.js
@@ -0,0 +1,82 @@
+// Programmatic Thinking
+
+// Problem-centric
+
+  // implementation
+    // the exact variables that I will need
+    // what I will specifically listen for (mouseEvent e.g. click, onchange)
+
+  // psuedo code
+
+// Problem: The train is too GD crowded (symptom)
+
+// Why: 
+  // - too many people (resources)
+  // - not enough trains (resources)
+  // - delays / contruction (one-off / semi-routine)
+  // - Air and Water Show (schedule-related / one-off)
+  // - Rush Hour (Schedule-related - recurring/routine)
+
+// Area of focus: Number of trains / Availability of trains
+
+// Solution: 
+  // - add more trains
+    // potential costs: 
+      // -scheduling conflicts
+      // - train jams (loop)
+      // - electricity (tax money)
+      // - increased labor
+      // - track wear (increased likelihood of maintenance)
+
+
+  // Add more trains
+
+
+
+    // Cost analysis
+      // research how to do cost analysis
+
+        // determine actual cost
+        
+        // determine actual intial revenue
+        // build fancy report
+
+
+
+
+
+
+
+    // city approval / budget allocation
+    // order dem young trains
+    // unbox the trains
+    // put them in service
+    // marketing / updating schedule
+
+
+// Build a programmatic thermostat
+
+
+
+
+// Introduction Functions
+
+function conjunction() {
+  // the work happens in here
+  alert("Tiffany is ON FIRE.")
+
+}
+
+// conjunction();
+
+
+
+
+
+  // parameters vs arguments
+
+// Event Listeners
+  // e.g. User Story: "I click a button, something happens."
+
+// Build a simple function
+// refactor that function to take dynamic inputs (parameters / arguments)
